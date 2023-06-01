1a21732df6d9aa2bdde75673e72a4b2f42eac8da
iHatesThisGame <64636625+iHatesThisGame@users.noreply.github.com>
2023-06-01 12:10:13 -0400

    Jump fixed, movement complete
    Will be adding shoot shortly, movement in and of itself is complete

--- Project_ShooteyShoot/Assets/Scripts/playerController.cs
+++ Project_ShooteyShoot/Assets/Scripts/playerController.cs
@@ -11,7 +11,9 @@ public class playerController : MonoBehaviour
     [Range(3, 8)][SerializeField] float playerSpeed;
     [Range(8, 25)][SerializeField] float jumpHeight;
     [Range(10, 50)][SerializeField] float gravityValue;
+    [SerializeField] int jumpMax;
 
+    private int jumpedTimes;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
     private Vector3 move;
@@ -27,14 +29,16 @@ public class playerController : MonoBehaviour
         if (groundedPlayer && playerVelocity.y < 0)
         {
             playerVelocity.y = 0f;
+            jumpedTimes = 0;
         }
 
         move = (transform.right * Input.GetAxis("Horizontal")) + (transform.forward * Input.GetAxis("Vertical"));
         controller.Move(move * Time.deltaTime * playerSpeed);
 
         // Changes the height position of the player..
-        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        if (Input.GetButtonDown("Jump") && jumpedTimes < jumpMax)
         {
+            jumpedTimes++;
             playerVelocity.y = jumpHeight;
         }
 
