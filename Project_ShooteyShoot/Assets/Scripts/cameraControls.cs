4e21b95f192bc0923bb549095aac5520d6cb2359
CLHeck <112427044+CLHeck@users.noreply.github.com>
2023-06-04 12:36:19 -0400

    Playing locks/hides cursor

--- Project_ShooteyShoot/Assets/Scripts/cameraControls.cs
+++ Project_ShooteyShoot/Assets/Scripts/cameraControls.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class cameraControls : MonoBehaviour
 {
@@ -14,7 +13,8 @@ public class cameraControls : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
