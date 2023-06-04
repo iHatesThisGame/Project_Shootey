402ab4dd49d86c4c0b3e70290c771a6b9765d071
CLHeck <112427044+CLHeck@users.noreply.github.com>
2023-06-04 13:18:50 -0400

    Menus & gameplay loop

---
+++ Project_ShooteyShoot/Assets/Scripts/buttonFunctions.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonFunctions : MonoBehaviour
+{
+    public void resume()
+    {
+        gameManager.instance.stateUnpaused();
+    }
+
+    public void Restart()
+    {
+        gameManager.instance.stateUnpaused();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Project_ShooteyShoot/Assets/Scripts/gameManager.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+    public static gameManager instance;
+
+    [Header("----- Player -----")]
+    public GameObject player;
+    public playerController playerController;
+
+    [Header("----- UI -----")]
+    public GameObject activeMenu;
+    public GameObject pauseMenu;
+    public GameObject winMenu;
+
+    [Header("----- Objective -----")]
+    int enemiesRemaining;
+
+    public bool isPaused;
+    float timescaleOrig;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        instance = this;
+        timescaleOrig = Time.timeScale;
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerController = player.GetComponent<playerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel") && activeMenu == null)
+        {
+            isPaused = !isPaused;
+            activeMenu = pauseMenu;
+            activeMenu.SetActive(isPaused);
+            statePaused();
+        }
+    }
+
+    public void statePaused()
+    {
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void stateUnpaused()
+    {
+        Time.timeScale = timescaleOrig;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        isPaused = !isPaused;
+        activeMenu.SetActive(false);
+        activeMenu = null;
+    }
+
+    public void updateGameGoal(int enemyCount)
+    {
+        enemiesRemaining += enemyCount;
+
+        if(enemiesRemaining <= 0)
+        {
+            //win con met
+            StartCoroutine(youWin());
+        }
+    }
+
+    IEnumerator youWin()
+    {
+        yield return new WaitForSeconds(2);
+        activeMenu = winMenu;
+        activeMenu.SetActive(true);
+        statePaused();
+    }
+}
