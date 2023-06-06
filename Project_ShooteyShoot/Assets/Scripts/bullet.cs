e183145052c92929f1be15d558582b84e0bae205
Keyonte Davis <keyonted00@gmail.com>
2023-06-06 02:59:19 -0500

    Bullet and Bullet Script Created

---
+++ Project_ShooteyShoot/Assets/Scripts/bullet.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    [SerializeField] int damage;
+    [SerializeField] int speed;
+    [SerializeField] int destroyTimer;
+
+    [SerializeField] Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, destroyTimer);
+        rb.velocity = transform.forward * speed;
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        IDamage damageble = other.GetComponent<IDamage>();
+
+        if (damageble != null )
+        {
+            damageble.takeDamage(damage);
+        }
+
+        Destroy(gameObject);
+    }
+}
