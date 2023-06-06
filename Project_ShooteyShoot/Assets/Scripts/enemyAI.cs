a5a432b79d2a141f905b5cb55ff8b12ecf329225
Keyonte Davis <keyonted00@gmail.com>
2023-06-05 20:33:32 -0500

    enemyAI code updated
    I updated the code to follow the player but it doesnt shoot yet unti the bullet function is added. which ill add.

--- Project_ShooteyShoot/Assets/Scripts/enemyAI.cs
+++ Project_ShooteyShoot/Assets/Scripts/enemyAI.cs
@@ -5,20 +5,123 @@ using UnityEngine.AI;
 
 public class enemyAI : MonoBehaviour
 {
+    [Header("----- Components -----")]
+    [SerializeField] Renderer model;
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Transform headPos;
+    [SerializeField] Transform shootPos;
 
-    public NavMeshAgent enemy;
+    [Header("----- Enemy Stats -----")]
+    [SerializeField] int HP;
+    [SerializeField] float speed;
+    [SerializeField] int playerFaceSpeed;
+    [SerializeField] int viewConeAngle;
 
-    public Transform Player;
+    [Header("----- Weapon Stats -----")]
+    [SerializeField] float shootRate;
+    [SerializeField] GameObject bullet;
+
+    Vector3 playerDir;
+    public bool playerInRange;
+    float angleToPlayer;
+    bool isShooting;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager.instance.updateGameGoal(1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        enemy.SetDestination(Player.position);
+        if (playerInRange && canSeePlayer()) 
+        {
+        
+        }
+    }
+
+    bool canSeePlayer()
+    {
+        playerDir = gameManager.instance.player.transform.position - headPos.position;
+        angleToPlayer = Vector3.Angle(new Vector3(playerDir.x, 0, playerDir.z), transform.forward);
+
+        Debug.DrawRay(headPos.position, playerDir);
+        Debug.Log(angleToPlayer);
+
+        RaycastHit hit;
+        if (Physics.Raycast(headPos.position, playerDir, out hit))
+        {
+
+            if (hit.collider.CompareTag("Player") && angleToPlayer <= viewConeAngle)
+            {
+                agent.SetDestination(gameManager.instance.player.transform.position);
+
+                if (agent.remainingDistance <= agent.stoppingDistance)
+                {
+                    facePlayer();
+                }
+
+                if (!isShooting)
+                {
+                    StartCoroutine(shoot());
+                }
+
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    IEnumerator shoot()
+    {
+        isShooting = true;
+
+        Instantiate(bullet, shootPos.position, transform.rotation);
+        yield return new WaitForSeconds(shootRate);
+        isShooting = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+
+    void facePlayer()
+    {
+        Quaternion rot = Quaternion.LookRotation(new Vector3(playerDir.x, 0, playerDir.z));
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * playerFaceSpeed);
+    }
+
+    public void takeDamage(int dmg)
+    {
+        HP -= dmg;
+        StartCoroutine(flashColor());
+
+        if (HP <= 0)
+        {
+            gameManager.instance.updateGameGoal(-1);
+            Destroy(gameObject);
+        }
+    }
+
+    IEnumerator flashColor()
+    {
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        model.material.color = Color.white;
     }
 }
