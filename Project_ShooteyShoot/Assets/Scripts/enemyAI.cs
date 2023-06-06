4854049c740d4b4eaf7257db991df9782b3556ce
Keyonte Davis <keyonted00@gmail.com>
2023-06-06 03:01:43 -0500

    Nothing Major

--- Project_ShooteyShoot/Assets/Scripts/enemyAI.cs
+++ Project_ShooteyShoot/Assets/Scripts/enemyAI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class enemyAI : MonoBehaviour
+public class enemyAI : MonoBehaviour, IDamage
 {
     [Header("----- Components -----")]
     [SerializeField] Renderer model;
