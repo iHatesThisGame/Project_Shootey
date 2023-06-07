ac13fd7d4f06d493e8d6ee357434e90b9a4b7da1
CLHeck <112427044+CLHeck@users.noreply.github.com>
2023-06-07 00:35:13 -0400

    Enemy shooting / bullet edits

--- Project_ShooteyShoot/Assets/Scripts/enemyAI.cs
+++ Project_ShooteyShoot/Assets/Scripts/enemyAI.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 public class enemyAI : MonoBehaviour, IDamage
 {
@@ -12,10 +13,12 @@ public class enemyAI : MonoBehaviour, IDamage
     [SerializeField] Transform shootPos;
 
     [Header("----- Enemy Stats -----")]
-    [SerializeField] int HP;
-    [SerializeField] float speed;
-    [SerializeField] int playerFaceSpeed;
-    [SerializeField] int viewConeAngle;
+    [Range(1, 10)] [SerializeField] int HP;
+    [Range(1, 10)] [SerializeField] float speed;
+    [Range(1, 10)] [SerializeField] int playerFaceSpeed;
+    [Range(1, 360)] [SerializeField] int viewConeAngle;
+    [Range(1, 100)] [SerializeField] int roamDist;
+    [Range(0, 10)] [SerializeField] int roamTimer;
 
     [Header("----- Weapon Stats -----")]
     [SerializeField] float shootRate;
@@ -25,25 +28,57 @@ public class enemyAI : MonoBehaviour, IDamage
     public bool playerInRange;
     float angleToPlayer;
     bool isShooting;
-
+    Vector3 startingPos;
+    bool destinationChosen;
+    float stoppingDistanceOrig;
 
     // Start is called before the first frame update
     void Start()
     {
         gameManager.instance.updateGameGoal(1);
+        startingPos = transform.position;
+        stoppingDistanceOrig = agent.stoppingDistance;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (playerInRange && canSeePlayer()) 
+        if (playerInRange && !canSeePlayer()) 
         {
-        
+            StartCoroutine(roam());
+        }
+        else if (agent.destination != gameManager.instance.player.transform.position)
+        {
+            StartCoroutine(roam());
+        }
+    }
+
+    IEnumerator roam()
+    {
+        if(!destinationChosen && agent.remainingDistance < 0.05f)
+        {
+            destinationChosen = true;
+
+            agent.stoppingDistance = 0;
+
+            yield return new WaitForSeconds(roamTimer);
+
+            destinationChosen = false;
+
+            Vector3 randomPos = Random.insideUnitSphere * roamDist;
+            randomPos += startingPos;
+
+            NavMeshHit hit;
+            NavMesh.SamplePosition(randomPos, out hit, roamDist, 1);
+
+            agent.SetDestination(hit.position);
         }
     }
 
     bool canSeePlayer()
     {
+        agent.stoppingDistance = stoppingDistanceOrig;
+
         playerDir = gameManager.instance.player.transform.position - headPos.position;
         angleToPlayer = Vector3.Angle(new Vector3(playerDir.x, 0, playerDir.z), transform.forward);
 
@@ -109,6 +144,7 @@ public class enemyAI : MonoBehaviour, IDamage
     public void takeDamage(int dmg)
     {
         HP -= dmg;
+        agent.SetDestination(gameManager.instance.player.transform.position);
         StartCoroutine(flashColor());
 
         if (HP <= 0)
