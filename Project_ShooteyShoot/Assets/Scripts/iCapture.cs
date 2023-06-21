aba0708bdff18de30bc7c61edb413651110e32e4
iHatesThisGame <64636625+iHatesThisGame@users.noreply.github.com>
2023-06-20 20:44:16 -0400

    ICapture and flag created

--- Project_ShooteyShoot/Assets/Scripts/iCapture.cs
+++ Project_ShooteyShoot/Assets/Scripts/iCapture.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class iCapture : MonoBehaviour
+public interface ICapture
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void capture(GameObject flag);
 }
