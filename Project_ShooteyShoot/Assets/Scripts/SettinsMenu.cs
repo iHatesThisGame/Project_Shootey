d678aa493731afe153905595c9ef7a352b19b6e8
MikeyBurgess9142 <MJBurgess@student.fullsail.edu>
2023-07-05 08:49:22 -0400

    Resolution and fullscreen settings work in main menu

--- Project_ShooteyShoot/Assets/Scripts/SettinsMenu.cs
+++ Project_ShooteyShoot/Assets/Scripts/SettinsMenu.cs
@@ -1,12 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class SettinsMenu : MonoBehaviour
 {
+    Resolution[] resolutions;
+    [SerializeField] TMP_Dropdown resDropdown;
+
+    public void Start()
+    {
+        resolutions = Screen.resolutions;
+        resDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+        int currResIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currResIndex = i;
+            }
+        }
+        resDropdown.AddOptions(options);
+        resDropdown.value = currResIndex;
+        resDropdown.RefreshShownValue();
+
+    }
 
     public void SetVolume(float vol)
     {
 
+    }
+
+    public void SetFullscreen(bool isFull)
+    {
+        Screen.fullScreen = isFull;
+    }
+
+    public void SetRes(int resIndex)
+    {
+        Resolution resolution = resolutions[resIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
