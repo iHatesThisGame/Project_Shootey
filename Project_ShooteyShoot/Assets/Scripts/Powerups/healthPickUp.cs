4e998b2cce9daf42c3a9261dab196110543a7cc3
Jesse Silcott <64636625+iHatesThisGame@users.noreply.github.com>
2023-07-16 10:45:32 -0500

    Powerups finished

--- Project_ShooteyShoot/Assets/Scripts/Powerups/healthPickUp.cs
+++ Project_ShooteyShoot/Assets/Scripts/Powerups/healthPickUp.cs
@@ -6,12 +6,19 @@ public class healthPickUp : MonoBehaviour
 {
     [SerializeField] GameObject healthPack;
 
+    [Header("----- Audio -----")]
+    [SerializeField] AudioSource aud;
+    [SerializeField] AudioClip audPickup;
+    [Range(0, 1)][SerializeField] float audPickupVol;
+
     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             if (gameManager.instance.playerController.HP != gameManager.instance.playerController.playerHPOrig)
             {
+                aud.PlayOneShot(audPickup, audPickupVol);
+
                 Destroy(healthPack);
                 gameManager.instance.playerController.HP = gameManager.instance.playerController.playerHPOrig;
                 gameManager.instance.playerController.updatePlayerUI(); 
