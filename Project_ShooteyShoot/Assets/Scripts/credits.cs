57ce4dae027fc2ccd4be3864b4189e1801660bd5
CLHeck <heckclinton@gmail.com>
2023-07-16 19:08:44 -0400

    Credits Roll

---
+++ Project_ShooteyShoot/Assets/Scripts/credits.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class credits : MonoBehaviour
+{
+    [SerializeField] int creditsDuration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(RollCredits());
+    }
+
+    IEnumerator RollCredits()
+    {
+        yield return new WaitForSeconds(creditsDuration);
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+        SceneManager.LoadScene("Main Menu");
+    }
+}
