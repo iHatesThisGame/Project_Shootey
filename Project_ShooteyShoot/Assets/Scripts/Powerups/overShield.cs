ee58ba11e4683ebf2b398135b6792ddf4af72a49
Jesse Silcott <64636625+iHatesThisGame@users.noreply.github.com>
2023-07-16 12:56:07 -0500

    Damage boost powerup sound fixed

--- Project_ShooteyShoot/Assets/Scripts/Powerups/overShield.cs
+++ Project_ShooteyShoot/Assets/Scripts/Powerups/overShield.cs
@@ -12,23 +12,49 @@ public class overShield : MonoBehaviour
     [SerializeField] AudioClip audPickup;
     [Range(0, 1)][SerializeField] float audPickupVol;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        IShield shielded = other.GetComponent<IShield>();
-        if (shielded != null)
-        {
-            if (gameManager.instance.playerController.shieldHP < gameManager.instance.playerController.shieldMax)
-            {
-                aud.PlayOneShot(audPickup, audPickupVol);
+    Vector3 despawn = new Vector3(-50, -50, -50);
 
-                Destroy(currOverShield);
-                gameManager.instance.playerController.shieldHP = shieldHP;
-                gameManager.instance.playerController.updatePlayerUI();
-                if (gameManager.instance.playerController.shieldHP > gameManager.instance.playerController.shieldMax)
-                {
-                    gameManager.instance.playerController.shieldHP = gameManager.instance.playerController.shieldMax;
-                } 
-            }
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    IShield shielded = other.GetComponent<IShield>();
+    //    if (shielded != null)
+    //    {
+    //        if (gameManager.instance.playerController.shieldHP < gameManager.instance.playerController.shieldMax)
+    //        {
+    //            aud.PlayOneShot(audPickup, audPickupVol);
+
+    //            currOverShield.transform.position -= despawn;
+
+    //            gameManager.instance.playerController.shieldHP = shieldHP;
+    //            gameManager.instance.playerController.updatePlayerUI();
+    //            if (gameManager.instance.playerController.shieldHP > gameManager.instance.playerController.shieldMax)
+    //            {
+    //                gameManager.instance.playerController.shieldHP = gameManager.instance.playerController.shieldMax;
+    //            }
+    //            Destroy(gameObject);
+    //        }
+    //    }
+    //}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(PowerupSequence());
         }
+    }
+
+    public IEnumerator PowerupSequence()
+    {
+        aud.PlayOneShot(audPickup, audPickupVol);
+
+        currOverShield.transform.position -= despawn;
+        gameManager.instance.playerController.shieldHP += shieldHP;
+        if (gameManager.instance.playerController.shieldHP > gameManager.instance.playerController.shieldMax)
+        {
+            gameManager.instance.playerController.shieldHP = gameManager.instance.playerController.shieldMax;
+        }
+        yield return new WaitForSeconds(1);
+        Destroy(currOverShield);
     }
 }
