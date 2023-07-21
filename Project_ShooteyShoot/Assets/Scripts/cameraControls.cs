9823ed682328234f11b34fd6d35366a195f4cd2f
MikeyBurgess9142 <MJBurgess@student.fullsail.edu>
2023-07-21 14:23:18 -0400

    camera sensitivity hooked up

--- Project_ShooteyShoot/Assets/Scripts/cameraControls.cs
+++ Project_ShooteyShoot/Assets/Scripts/cameraControls.cs
@@ -1,20 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class cameraControls : MonoBehaviour
 {
-    [SerializeField] int sensitivity;
+    public int sensitivity;
+    [SerializeField] Slider sensitivitySlider;
+    [SerializeField] string parSensitivity = "Mouse Sensitivity";
     [SerializeField] int lockVerMin;
     [SerializeField] int lockVerMax;
     [SerializeField] bool inverY;
     float xRotation;
+
+    private void Awake()
+    {
+        sensitivitySlider.onValueChanged.AddListener(SensSlideValChange);
+    }
+
+    private void SensSlideValChange(float arg0)
+    {
+        sensitivity = (int)sensitivitySlider.value;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        PlayerPrefs.GetFloat(parSensitivity, sensitivitySlider.value);
     }
 
     // Update is called once per frame
@@ -32,5 +47,14 @@ public class cameraControls : MonoBehaviour
         //rotate x and y axis'
         transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
         transform.parent.Rotate(Vector3.up * mouseX);
+
+        if (sensitivity == PlayerPrefs.GetFloat(parSensitivity, sensitivitySlider.value) )
+        {
+            return;
+        }
+        else
+        {
+            PlayerPrefs.SetFloat(parSensitivity, sensitivitySlider.value);
+        }
     }
 }
